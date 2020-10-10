8c4b834f6e8edc314c7020398c85b7a2500489ed
Prisca Roseline <prisca.ros@onja.org>
2020-10-10 12:01:21 +0300

    I layed out the button cmponent

--- Component/App.js
+++ Component/App.js
@@ -3,112 +3,70 @@ import DefaultBtn from "./DefaultBtn";
 
 function App() {
   return (
-    <div>
+    <section>
       <div className="container">
-        <div className="wrapper">
-          <p>{"<button />"}</p>
-          <DefaultBtn>Default</ DefaultBtn>
-        </div>
-        <div className="wrapper">
-          <p>{"&hover &focus"}</p>
-          <DefaultBtn>Default</ DefaultBtn>
-        </div>
+        <p>{"<button />"}</p>
+        <DefaultBtn>Default</ DefaultBtn>
       </div>
       <div className="container">
-        <div className="wrapper">
-          <p>{`<button variant="outline" />`}</p>
-          <DefaultBtn variant="outline" disabledShadow="none">Default</ DefaultBtn>
-        </div>
-        <div className="wrapper">
-          <p>{`&hover &focus`}</p>
-          <DefaultBtn variant="outline" disabledShadow="none">Default</ DefaultBtn>
-        </div>
+        <p>{`<button variant="outline" />`}</p>
+        <DefaultBtn variant="outline" disabledShadow="none">Default</ DefaultBtn>
       </div>
       <div className="container">
-        <div className="wrapper">
-          <p>{`<button variant="text" />`}</p>
-          <DefaultBtn variant="text" disabledShadow="none">Default</ DefaultBtn>
-        </div>
-        <div className="wrapper">
-          <p>{`&hover &focus`}</p>
-          <DefaultBtn variant="text" disabledShadow="none">Default</ DefaultBtn>
-        </div>
+        <p>{`<button variant="text" />`}</p>
+        <DefaultBtn variant="text" disabledShadow="none">Default</ DefaultBtn>
       </div>
-      <div className="wrapper">
+      <div className="container">
         <p>{`<button disabledShadow />`}</p>
         <DefaultBtn disabledShadow="none" color="primary" size="lg">Default</ DefaultBtn>
       </div>
       <div className="container">
-        <div className="wrapper">
-          <p>{`<button disabled />`}</p>
-          <DefaultBtn disabled="yes">Desabled</ DefaultBtn>
-        </div>
-        <div className="wrapper">
-          <p>{`variant="text" disabled`}</p>
-          <DefaultBtn variant="text" disabled="yes" disabledShadow="none">Desabled</ DefaultBtn>
-        </div>
+        <p>{`<button disabled />`}</p>
+        <DefaultBtn disabled="yes">Desabled</ DefaultBtn>
       </div>
-      <div className="container">
-        <div className="wrapper">
+      <div className="container wrapper">
+        <div>
           <p>{`<button startIcon="local_grocery_store" />`}</p>
           <DefaultBtn startIcon="local_grocery_store" color="primary" size="lg" disabledShadow="none">Default</ DefaultBtn>
         </div>
-        <div className="wrapper">
+        <div>
           <p>{`<button endIcon="local_grocery_end-icon" />`}</p>
           <DefaultBtn endIcon="local_grocery_store" color="primary" size="lg" disabledShadow="none">Default</ DefaultBtn>
         </div>
       </div>
-      <div className="container">
-        <div className="wrapper">
+      <div className="container wrapper">
+        <div>
           <p>{`<button sise="sm" />`}</p>
           <DefaultBtn size="sm" color="primary" disabledShadow="none">Default</ DefaultBtn>
         </div>
-        <div className="wrapper">
+        <div>
           <p>{`<button sise="md" />`}</p>
           <DefaultBtn size="md" color="primary" disabledShadow="none">Default</ DefaultBtn>
         </div>
-        <div className="wrapper">
+        <div>
           <p>{`<button sise="lg"/>`}</p>
           <DefaultBtn size="lg" color="primary">Default</ DefaultBtn>
         </div>
       </div>
-      <div className="container">
-        <div className="wrapper">
+      <div className="container wrapper">
+        <div>
           <p>{`<button default />`}</p>
           <DefaultBtn color="default">Default</ DefaultBtn>
         </div>
-        <div className="wrapper">
+        <div>
           <p>{`<button color="primary"/>`}</p>
           <DefaultBtn color="primary" disabledShadow="none">primary</ DefaultBtn>
         </div>
-        <div className="wrapper">
+        <div>
           <p>{`<button color="secondary" />`}</p>
           <DefaultBtn color="secondary" disabledShadow="none">secondary</ DefaultBtn>
         </div>
-        <div className="wrapper">
+        <div>
           <p>{`<button color="danger" />`}</p>
           <DefaultBtn color="danger" disabledShadow="none">Danger</ DefaultBtn>
         </div>
       </div>
-      <div className="container">
-        <div className="wrapper">
-          <p>{`&hover &focus`}</p>
-          <DefaultBtn color="default">Default</ DefaultBtn>
-        </div>
-        <div className="wrapper">
-          <p>{`&hover &focus`}</p>
-          <DefaultBtn color="primary" disabledShadow="none">primary</ DefaultBtn>
-        </div>
-        <div className="wrapper">
-          <p>{`&hover &focus`}</p>
-          <DefaultBtn color="secondary" disabledShadow="none">secondary</ DefaultBtn>
-        </div>
-        <div className="wrapper">
-          <p>{`&hover &focus`}</p>
-          <DefaultBtn color="danger" disabledShadow="none">Danger</ DefaultBtn>
-        </div>
-      </div>
-    </div>
+    </section>
   )
 }
 
